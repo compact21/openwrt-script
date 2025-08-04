93bfabca2b3b45321d9e39efb19395f29ce8a854
compact21 <139592405+compact21@users.noreply.github.com>
2025-08-05 00:58:36 +0200

    Create script4.sh

---
+++ temp/script4.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# if it runs with a crontab prevent it from being re-executed if file /tmp/script4.lock exists
+# as I have no idea if there is a possibility to run a crontab with flock on this system
+# https://man7.org/linux/man-pages/man1/flock.1.html
+#
+# cat /etc/crontabs/root
+# * * * * * /home/root/script4.sh
+# * * * * * flock -n /tmp/script4.lock /home/root/script4.sh
+#
+
+if [ ! -f /tmp/script4.lock ]; then
+  ping -c 1 -W 1 8.8.8.8
+  if [ $? -ne "0" ]; then
+    sleep 2
+    ping -c 1 -W 1 1.1.1.1
+      if [ $? -ne "0" ]; then
+      touch /tmp/script4.lock
+      sys resetcm
+      sleep 300
+      rm /tmp/script4.lock
+      fi
+  fi
+fi
