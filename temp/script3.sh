9fbd99607bee680f8619d4229a2b3665fdca3198
compact21 <139592405+compact21@users.noreply.github.com>
2025-08-05 01:10:24 +0200

    Update script3.sh

--- temp/script3.sh
+++ temp/script3.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# interval greater than the sum of the sleeps = 302 seconds
+#
+# if you want to run it with a crontab add this line into /etc/crontabs/root
+#
+# cat /etc/crontabs/root
+# */6 * * * * /home/root/script3.sh
+
 ping -c 1 -W 1 8.8.8.8
 if [ $? -ne "0" ]; then
     sleep 2
