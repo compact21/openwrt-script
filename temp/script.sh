d50dc83dea5105e3b5187a064ee35294e1d1691d
compact21 <139592405+compact21@users.noreply.github.com>
2025-08-06 09:30:02 +0200

    Update script.sh

--- temp/script.sh
+++ temp/script.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+# for 600 sec from boot not exec nothing
+statuptime=$(cut -f1 -d. /proc/uptime)
+if [ "$statuptime" -lt "600" ]; then
+exit 0
+fi
+
 touch /tmp/script.lock
 udhcpc -i wwan0 > /tmp/script.lock 2>&1
