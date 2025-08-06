da686389bf78e30749a8ecaf648bcb61da8eb384
compact21 <139592405+compact21@users.noreply.github.com>
2025-08-06 23:00:43 +0200

    Update script2.sh

--- temp/script2.sh
+++ temp/script2.sh
@@ -6,5 +6,8 @@ if [ "$statuptime" -lt "600" ]; then
 exit 0
 fi
 
-touch /tmp/script2.lock
-sys resetcm > /tmp/script2.lock 2>&1
+#touch /tmp/script2.lock
+#sys resetcm > /tmp/script2.lock 2>&1
+
+cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 0
+cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 1
