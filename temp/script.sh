80e12c6884993d2701d4a96c49f4da2b7e482893
compact21 <139592405+compact21@users.noreply.github.com>
2025-08-06 22:56:09 +0200

    Update script.sh

--- temp/script.sh
+++ temp/script.sh
@@ -6,5 +6,9 @@ if [ "$statuptime" -lt "600" ]; then
 exit 0
 fi
 
-touch /tmp/script.lock
-udhcpc -i wwan0 > /tmp/script.lock 2>&1
+#touch /tmp/script.lock
+
+#udhcpc -i wwan0 > /tmp/script.lock 2>&1
+
+cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 0
+cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 1
