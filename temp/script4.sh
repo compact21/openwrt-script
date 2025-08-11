82c9828f33cc05fff05864a5b6a2a8c7681d932f
compact21 <139592405+compact21@users.noreply.github.com>
2025-08-11 21:58:40 +0200

    Update script4.sh

--- temp/script4.sh
+++ temp/script4.sh
@@ -6,28 +6,29 @@
 # */2 * * * * /home/root/script4.sh
 
 # for 600 sec from boot not exec nothing
-statuptime=$(cut -f1 -d. /proc/uptime)
-if [ "$statuptime" -lt "600" ]; then
-exit 0
-fi
+#statuptime=$(cut -f1 -d. /proc/uptime)
+#if [ "$statuptime" -lt "600" ]; then
+#exit 0
+#fi
 
 # prevent exectution this script, actual run /home/root/scritp3.sh at 2:00
 if [ -f /tmp/script3.lock ]; then
-logger "warning I can't do anything because I'm already doing it /home/root/script3.sh"
+#logger "warning I can't do anything because I'm already doing it /home/root/script3.sh"
 exit 0
 fi
 
 if [ ! -f /tmp/script4.lock ]; then
-  ping -c 1 -W 1 8.8.8.8
+  ping -c 1 -W 10 8.8.8.8
   if [ $? -ne "0" ]; then
     sleep 2
-    ping -c 1 -W 1 1.1.1.1
+    ping -c 1 -W 10 1.1.1.1
       if [ $? -ne "0" ]; then
       touch /tmp/script4.lock
-      logger "exec: /home/root/script4.sh"
+      #logger "exec: /home/root/script4.sh"
       cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 0
+      sleep 2
       cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 1
-      sleep 300
+      sleep 120
       rm /tmp/script4.lock
       fi
   fi
