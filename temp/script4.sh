8bdc0431c24e773d21a1494ccd5b26df13edf2ed
compact21 <139592405+compact21@users.noreply.github.com>
2025-08-11 22:50:31 +0200

    Update script4.sh

--- temp/script4.sh
+++ temp/script4.sh
@@ -5,14 +5,17 @@
 # cat /etc/crontab.x/root
 # */2 * * * * /home/root/script4.sh
 
-# for 600 sec from boot not exec nothing
+## for 600 sec from boot not exec nothing
 #statuptime=$(cut -f1 -d. /proc/uptime)
 #if [ "$statuptime" -lt "600" ]; then
 #exit 0
 #fi
 
+logger "exec /home/root/script4.sh"
+
 # prevent exectution this script, actual run /home/root/scritp3.sh at 2:00
 if [ -f /tmp/script3.lock ]; then
+logger "warning run /home/root/script3.sh; exit 0"
 #logger "warning I can't do anything because I'm already doing it /home/root/script3.sh"
 exit 0
 fi
@@ -24,12 +27,17 @@ if [ ! -f /tmp/script4.lock ]; then
     ping -c 1 -W 10 1.1.1.1
       if [ $? -ne "0" ]; then
       touch /tmp/script4.lock
-      #logger "exec: /home/root/script4.sh"
+      logger "exec: /home/root/script4.sh cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 0"
       cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 0
       sleep 2
+      logger "exec: /home/root/script4.sh cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 1"
       cfg cellwan_mapn edit --Index 1 --AP_ManualAPN 1
       sleep 120
       rm /tmp/script4.lock
+      else
+      logger "ping -c 1 -W 10 1.1.1.1 good"
       fi
+  else
+  logger "ping -c 1 -W 10 8.8.8.8 good"
   fi
 fi
